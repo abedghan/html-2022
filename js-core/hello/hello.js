d35bc8c8c8ea645593646cc9fea778dd883f214b
abed <abed@abed.com>
2022-01-18 20:51:07 +0200

    curs1-java

---
+++ js-core/hello/hello.js
@@ -0,0 +1,8 @@
+
+    //ruleaza tellMe dupa3000 ms (3 secunde):
+setTimeout(tellMe, 3000)
+// creaza o noua instructiune (functie) numite tellMe
+function tellMe() {
+    alert("Hello world!")
+    alert("welcome to javescript")
+}
